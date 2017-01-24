eefa623e5be3a56830389970c8385866405b8e26
Peter Hudec <peterhudec@peterhudec.com>
2017-01-24 19:26:08 +0000

    Fixed the wrong bottom name.

--- src/cubes/bottom-name.js
+++ src/cubes/bottom-name.js
@@ -8,24 +8,20 @@ const ph = []
   .concat(utils.shiftCubesBy(0, 0, 0, utils.bar3x1x1))
   .concat(utils.shiftCubesBy(0, 2, 0, utils.bar3x1x1))
 
-  .concat(utils.shiftCubesBy(-2, -2, 0, utils.bar1x1x5))
-  .concat(utils.shiftCubesBy(-2, -1, 0, utils.bar1x1x5))
-  .concat(utils.shiftCubesBy(-2, 0, 0, utils.bar1x1x5))
-  .concat(utils.shiftCubesBy(-2, 2, 0, utils.bar1x1x5))
+  .concat(utils.shiftCubesBy(-2, -2, 2, utils.bar1x2x1))
+  .concat(utils.shiftCubesBy(-2, 0, 1, utils.bar1x1x3))
+  .concat(utils.shiftCubesBy(-2, 2, -1, utils.bar1x1x3))
 
   .concat(utils.shiftCubesBy(2, 2, 1, utils.bar1x1x3))
-  .concat(utils.shiftCubesBy(2, 1, 1, utils.bar1x1x3))
+  .concat(utils.shiftCubesBy(2, 1, 2, utils.bar1x1x1))
   .concat(utils.shiftCubesBy(2, 0, 1, utils.bar1x1x3))
 
-const flatU = []
-  .concat(utils.shiftCubesBy(-2, 0, 0, utils.bar1x1x5))
-  .concat(utils.shiftCubesBy(0, 0, 2, utils.bar3x1x1))
-  .concat(utils.shiftCubesBy(2, 0, 0, utils.bar1x1x5))
-
 const eu = []
-  .concat(utils.shiftCubesBy(0, -2, 0, flatU))
-  .concat(utils.shiftCubesBy(0, 0, 0, flatU))
-  .concat(utils.shiftCubesBy(0, 2, 0, flatU))
+  .concat(utils.shiftCubesBy(0, -2, 2, utils.bar5x1x1))
+  .concat(utils.shiftCubesBy(0, 0, 2, utils.bar5x1x1))
+  .concat(utils.shiftCubesBy(0, 2, 2, utils.bar5x1x1))
+  .concat(utils.shiftCubesBy(-2, 2, 0, utils.bar1x1x4))
+  .concat(utils.shiftCubesBy(2, -2, 0, utils.bar1x1x4))
 
 const td = []
   .concat(utils.shiftCubesBy(0, 2, 2, utils.bar5x1x1))
@@ -33,24 +29,15 @@ const td = []
   .concat(utils.shiftCubesBy(-2, 2, 1, utils.bar1x1x1))
   .concat(utils.shiftCubesBy(2, 2, -2, utils.bar1x1x2))
   .concat(utils.shiftCubesBy(0, 0, 0, utils.bar1x4x1))
-  .concat(utils.shiftCubesBy(0, 0, 2, utils.bar1x4x1))
-
-const flatE = []
-.concat(utils.shiftCubesBy(0, 0, 2, utils.bar5x1x1))
-.concat(utils.shiftCubesBy(0, 0, 0, utils.bar5x1x1))
-.concat(utils.shiftCubesBy(0, 0, -2, utils.bar5x1x1))
 
 const ee = []
-  .concat(utils.shiftCubesBy(0, -2, 0, flatE))
-  .concat(utils.shiftCubesBy(0, 0, 0, flatE))
-  .concat(utils.shiftCubesBy(0, 2, 0, flatE))
+  .concat(utils.shiftCubesBy(0, 2, -2, utils.bar5x1x1))
+  .concat(utils.shiftCubesBy(0, 0, 0, utils.bar5x1x1))
+  .concat(utils.shiftCubesBy(0, -2, 2, utils.bar5x1x1))
 
 const rc = []
-  .concat(utils.shiftCubesBy(-2, -2, 0, utils.bar1x1x5))
-  .concat(utils.shiftCubesBy(-2, -1, 0, utils.bar1x1x5))
-  .concat(utils.shiftCubesBy(-2, 0, 0, utils.bar1x1x5))
-  .concat(utils.shiftCubesBy(-2, 1, 0, utils.bar1x1x5))
   .concat(utils.shiftCubesBy(-2, 2, 0, utils.bar1x1x5))
+  .concat(utils.shiftCubesBy(-2, 0, 0, utils.bar1x4x1))
 
   .concat(utils.shiftCubesBy(1, 2, -2, utils.bar4x1x1))
   .concat(utils.shiftCubesBy(1, 2, 2, utils.bar4x1x1))
