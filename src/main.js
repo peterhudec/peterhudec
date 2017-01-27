c49c07f0e692ffb96a0640b7841ed56d1d6b71c0
Peter Hudec <peterhudec@peterhudec.com>
2017-01-27 16:33:52 +0000

    Fixed Internet Explorer compatibility issues.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+console.log('a')
+
 import $ from 'jquery'
 import Detector from 'three/examples/js/Detector'
 
@@ -8,6 +10,10 @@ $(document).ready(() => {
   if (Detector.webgl) {
     app()
   } else {
-    $('body').html($('noscript').text())
+    $('body')
+      .html($('noscript').text())
+      .prepend($('<p class="browser-admonition"></p>')
+        .text('You would enjoy this website much more on a modern browser ' +
+          'with WebGL support.'))
   }
 })
